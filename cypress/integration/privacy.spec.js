161e2dc6b2a11c17596d3b6b1faa9341518b1614
Gustavo Barros <gustavo.barros@valtech.com>
2022-08-09 23:07:32 -0300

    comandos avançados Cypress

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,7 @@
-it('testa a pagina da politica de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
-
-    cy.contains('Talking About Testing').should('be.visible')
+Cypress._.times(3, function(){
+    it('testa a pagina da politica de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
+    
+        cy.contains('Talking About Testing').should('be.visible')
+    })
 })
